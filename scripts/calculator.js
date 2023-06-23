a6a2cb0d89d47aeb86ee72e555ffc5bd00452d9d
rodolfoarambulo <108365768+rodolfoarambulo@users.noreply.github.com>
2023-06-23 20:27:59 +0800

    Add operate function

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,3 +1,25 @@
+let num1,
+    num2,
+    operator;
+
+function operate(num1, operator, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    }
+
+    if (operator === '-') {
+        return subtract(num1, num2);
+    }
+
+    if (operator === '*') {
+        return multiply(num1, num2);
+    }
+
+    if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
+
 function add(a, b) {
     return a + b;
 }
